3075453946374e65481d15b10e191cb39e8acc2c
henriettepilegaard <201507843@post.au.dk>
2019-02-13 12:35:08 +0100

    Creating all classes and interfases

--- Legacy2/Application2.cs
+++ Legacy2/Application2.cs
@@ -8,5 +8,15 @@ namespace Legacy2
 {
     class Application2
     {
+        public static void Main(string[] args)
+        {
+            //var ecs = new ECS(28);
+
+            //ecs.Regulate();
+
+            //ecs.SetThreshold(20);
+
+            //ecs.Regulate();
+        }
     }
 }
