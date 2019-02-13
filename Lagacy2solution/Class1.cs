fed91290eacb7e1480fb7f2a8132b5a7003b134b
henriettepilegaard <201507843@post.au.dk>
2019-02-13 11:27:24 +0100

    Creation solutuin 1.1

---
+++ Lagacy2solution/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagacy2solution
+{
+    public class Class1
+    {
+    }
+}
